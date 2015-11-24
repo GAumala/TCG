fe79655d61afa73fe0be8447a9d1f3095282bd69
Gabriel Aumala <gabrielfima@live.com>
2015-11-24 01:40:41 -0500

    Integracion con FacebookSDK
    Se integro el SDK de facebook. Por ahora se puede enviar una foto por facebook.
    
    Para facilitar el share, Los activites que van a compartir informacion deben de
    extender ShareActivity en lugar de AppCompatActivity. Esta clase ya tiene los
    metodos y atributos para conectarse con facebook.

--- app/src/main/java/ec/orangephi/tcg/CardViewActivity.java
+++ app/src/main/java/ec/orangephi/tcg/CardViewActivity.java
@@ -1,17 +1,22 @@
 package ec.orangephi.tcg;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CardViewActivity extends AppCompatActivity {
+import ec.orangephi.tcg.utils.ShareUtils;
+
+public class CardViewActivity extends ShareActivity {
 
     public static String CardCode = "CardViewActivity.CardCode";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -21,8 +26,8 @@ public class CardViewActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Bitmap bmp = ((BitmapDrawable)ContextCompat.getDrawable(CardViewActivity.this, R.drawable.ajna1)).getBitmap();
+                sharePhotoFB(bmp);
             }
         });
     }
